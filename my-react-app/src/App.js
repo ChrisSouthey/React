46f9a1a0c07cafeeb16cf10901aa515b50d2d594
ChrisSouthey <chsouthey@email.neit.edu>
2025-03-26 21:36:58 -0400

    yipiie

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ToDo from './ToDo';
+import { Modal } from './modal';
+
+function App() {
+  return (
+      <div class="todo-container">
+        <div class="card">
+            <ToDo></ToDo>
+
+        </div>
+      </div>
+  );
+}
+
+export default App;
